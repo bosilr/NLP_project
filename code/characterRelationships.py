c95916279e1117c8d2b47697f29c57fffd063dee
Rok Bosil <rb2600@student.uni-lj.si>
2022-05-17 10:33:53 +0200

    Update

--- code/characterRelationships.py
+++ code/characterRelationships.py
@@ -39,7 +39,7 @@ def NER(s):
 
 
 def get_named_entities(sentences):
-    threshold = 0.0001
+    threshold = 0.0005
     named_entities = []
     named_entities_thresholded = []
     for s in sentences:
@@ -85,6 +85,43 @@ def get_mtx(sentences, main_characters):
 
     return sentiment_mtx, cooccur_mtx
 
+def matrix_to_edge_list(matrix, main_characters, mode):
+    edge_list = []
+    shape = matrix.shape[0]
+    lower_tri_loc = list(zip(*np.where(np.triu(np.ones([shape, shape])) == 0)))
+    normalized_matrix = matrix / np.max(np.abs(matrix))
+    if mode == 'co-occurrence':
+        weight = np.log(2000 * normalized_matrix + 1) * 0.7
+        color = np.log(2000 * normalized_matrix + 1)
+    if mode == 'sentiment':
+        weight = np.log(np.abs(1000 * normalized_matrix) + 1) * 0.7
+        color = 2000 * normalized_matrix
+    for i in lower_tri_loc:
+        edge_list.append((main_characters[i[0]], main_characters[i[1]], {'weight': weight[i], 'color': color[i]}))
+
+    return edge_list
+
+
+def plotGraph(edge_list, nor_freq, main_characters, name):
+    plt.figure(figsize=(20, 20))
+    G = nx.Graph()
+    G.add_nodes_from(main_characters)
+    G.add_edges_from(edge_list)
+
+    weights = [G[u][v]['weight'] for u, v in G.edges]
+    colors = [G[u][v]['color'] for u, v in G.edges]
+
+    label = {i: i for i in main_characters}
+    if name == 'Sentiment':
+        nx.draw(G, nx.circular_layout(G), node_color='#A0CBE2', node_size=np.sqrt(nor_freq) * 4000,
+                linewidths=10, font_size=35, labels=label, edge_color=colors, with_labels=True,
+                width=weights, edge_vmin=-1000, edge_vmax=1000)
+    else:
+        nx.draw(G, nx.circular_layout(G), node_color='#A0CBE2', node_size=np.sqrt(nor_freq) * 4000, edge_cmap=plt.cm.Blues,
+                linewidths=10, font_size=35, labels=label, edge_color=colors, with_labels=True, width=weights)
+
+    plt.savefig(name+'.png')
+
 
 if __name__ == "__main__":
     nlp = spacy.load('en_core_web_sm')
@@ -96,9 +133,15 @@ if __name__ == "__main__":
     characters = get_named_entities(sentences)
     print(characters)
 
-    freq, main_characters = get_main_characters(book, characters, number_of_characters=10)
+    freq, main_characters = get_main_characters(book, characters, number_of_characters=25)
 
     sentiment_mtx, cooccur_mtx = get_mtx(sentences, main_characters)
 
     # TODO: plotting
+    # plotting
+    edge_list_sentiment = matrix_to_edge_list(sentiment_mtx, main_characters, mode='co-occurrence')
+    edge_list_cooccur = matrix_to_edge_list(cooccur_mtx, main_characters, mode='sentiment')
+    nor_freq = np.array(freq) / np.max(freq)
 
+    plotGraph(edge_list_sentiment, nor_freq, main_characters, name='Sentiment')
+    plotGraph(edge_list_cooccur, nor_freq, main_characters, name='Coocur')
